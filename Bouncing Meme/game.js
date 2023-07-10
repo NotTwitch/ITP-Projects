7e996afe5fa434a617fe3e6661e5db402d44fedb
NotTwitch <137024472+NotTwitch@users.noreply.github.com>
2023-07-10 01:22:44 -0400

    Add files via upload

---
+++ Bouncing Meme/game.js
@@ -0,0 +1,71 @@
+//Rendering stuff
+const screenWidth = 640;
+const screenHeight = 480;
+  
+const frameDelay = 1000/60;
+
+const canvas = document.createElement("canvas");
+const context = canvas.getContext("2d");
+
+//Image
+const dvd = new Image();
+dvd.src = "dvd.png"
+
+//Sound
+const bump = new Audio();
+bump.src = "bump.wav";
+
+//Movement Variables
+var x = 0;
+var y = 0;
+var vx = 0;
+var vy = 0;
+
+//Starting the canvas
+canvas.setAttribute("width", screenWidth);
+canvas.setAttribute("height", screenHeight);
+document.body.appendChild(canvas);
+setInterval(Update,frameDelay)
+
+//Starts moving the meme
+function Start()
+{
+  vx = 1;
+  vy = 1;
+}
+
+//Stops the meme from moving
+function Stop()
+{
+  vx = 0;
+  vy = 0;
+}
+
+//Updates every frame so the meme moves
+function Update()
+{
+  //Adding velocity to position
+  x += vx;
+  y += vy;
+
+  //if the meme hits left or right edge, invert velocity
+  if(x >= (screenWidth - 50) || x <= 0)
+  {
+    vx = -vx;
+    bump.play();
+  }
+
+  //if the meme hits top or bottom, invert velocity
+  if(y >= (screenHeight - 50) || y <= 0)
+  {
+    vy = -vy;
+    bump.play();
+  }
+
+  //clear the screen
+  context.fillStyle = "grey";
+  context.fillRect(0,0,screenWidth,screenHeight);
+
+  //render the meme
+  context.drawImage(dvd,x,y,50,50);
+}
